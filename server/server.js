12eff914fec16ff1bfe5ca2231e477513adc5eec
tanvirtbf <tanver018765@gmail.com>
2024-11-18 11:34:34 +0600

    basic setup client and server

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+// create a database connection
+mongoose
+  .connect(
+    "mongodb+srv://tanver_tbf:cvFxTExB5IjBBcPS@cluster0.nsurs.mongodb.net/"
+  )
+  .then(() => console.log("MongoDB Connected.."))
+  .catch((err) => console.log(err));
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173/',
+    methods: ['GET','POST','DELETE','PUT'],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma"
+    ],
+    credentials: true,
+  })
+)
+
+app.use(cookieParser())
+
+app.use(express.json())
+
+// Run Server
+app.listen(PORT, ()=> {
+  console.log(`Server Running on ${PORT}`)
+})
