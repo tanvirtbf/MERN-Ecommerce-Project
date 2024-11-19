8a5375c76b5380b74c02db410f5985d23b534a22
tanvirtbf <tanver018765@gmail.com>
2024-11-19 15:45:44 +0600

    login page complete here

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -1,10 +1,42 @@
+import CommonForm from "@/components/common/form";
+import { loginFormControls } from "@/config";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 const AuthLogin = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const [formData, setFormData] = useState(initialState);
 
-export default AuthLogin
+  const onSubmit = () => {};
+
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Sign in to your account
+        </h1>
+        <p className="mt-2">Don't have an accout?</p>
+        <Link
+          className="font-medium text-primary hover:underline ml-2"
+          to={"/auth/register"}
+        >
+          Register
+        </Link>
+      </div>
+      <CommonForm
+        formControls={loginFormControls}
+        buttonText={"Sign In"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
+};
+
+export default AuthLogin;
+
