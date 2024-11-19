ab8e2eb38aeace581d275138690b302feffd67a3
tanvirtbf <tanver018765@gmail.com>
2024-11-19 17:09:17 +0600

    registerUser controller created

---
+++ server/controllers/auth/auth-controller.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const User = require('../../models/User');
+
+
+//Register
+const registerUser = async (req, res) => {
+  const { userName, email, password } = req.body;
+
+
+
+  try {
+
+    const hashPassword = await bcrypt.hash(password, 12)
+
+    const newUser = new User({
+      userName, email, password: hashPassword
+    })
+    await newUser.save()
+
+    res.status(200).json({
+      success : true,
+      message : 'Registration Successful'
+    })
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+};
+
+//Login
+const login = async (req, res) => {
+  const { userName, email, password } = req.body;
+
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+};
+
+//Logout
+
+
+
+
+module.exports = { registerUser }
