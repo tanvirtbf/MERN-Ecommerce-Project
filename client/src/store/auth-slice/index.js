16dc22142014dac48683c7bc765f2005cdffdd9a
tanvirtbf <tanver018765@gmail.com>
2024-11-20 10:32:35 +0600

    registerUser async thunk create

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -27,6 +27,19 @@ const authSlice = createSlice({
   reducers: {
     setUser: (state, action) => {},
   },
+  extraReducers : (builder) => {
+    builder.addCase(registerUser.pending, (state) => {
+      state.isLoading = true
+    }).addCase(registerUser.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.user = action.payload
+      state.isAuthenticated = false
+    }).addCase(registerUser.rejected, (state, action) => {
+      state.isLoading = false
+      state.user = null
+      state.isAuthenticated = false
+    })
+  }
 });
 
 export const { setUser } = authSlice.actions;
